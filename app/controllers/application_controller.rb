8a25a0c3124e0853ae82335482dce7466cf0c3c4
apecherin <artempecherin@gmail.com>
2015-04-05 03:00:05 +0300

    protect_from_bots

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :authenticate
 
   def index
     if current_user
@@ -14,5 +15,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :username) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :current_password, :password, :password_confirmation, :username) }
   end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      (username == "artemon" && password == "pecherin")
+    end if Rails.env.production?
+  end
+
   protect_from_forgery with: :exception
 end
